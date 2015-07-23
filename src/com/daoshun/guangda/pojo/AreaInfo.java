11e733667aabb1607a1517e059f3d62fac5f5674
laolujava <laolujava@126.com>
2015-07-23 10:46:03 +0800

    修改省市区POJO类名称

--- src/com/daoshun/guangda/pojo/AreaInfo.java
+++ src/com/daoshun/guangda/pojo/AreaInfo.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "t_areas")
-public class Areas {
+public class AreaInfo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", length = 20, nullable = false)
--- src/com/daoshun/guangda/pojo/ProvinceInfo.java
+++ src/com/daoshun/guangda/pojo/ProvinceInfo.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "t_provinces")
-public class Provinces {
+public class ProvinceInfo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", length = 20, nullable = false)
