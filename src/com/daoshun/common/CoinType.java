bc895a3661e28363edae9865f4b679fe53030e1e
laolujava <laolujava@126.com>
2015-08-03 15:12:33 +0800

    添加小巴币类型常量

---
+++ src/com/daoshun/common/CoinType.java
@@ -0,0 +1,24 @@
+package com.daoshun.common;
+/**
+ * 小巴币记录类型常量
+ * @author 卢磊
+ *
+ */
+public class CoinType {
+	/**
+	 * 发放给学员
+	 */
+	public static int TO_STUDENT=1;
+	/**
+	 * 学员支付
+	 */
+    public static int STUDENT_PAY=2;
+    /**
+     * 退款
+     */
+    public static int REFUND=3;
+    /**
+     * 教练兑换	
+     */
+    public static int COACH_EXCHANGE=4;
+}
