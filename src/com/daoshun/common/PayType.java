5737cccae8e723bfdd35ba0042c06f1a09a7d588
laolujava <laolujava@126.com>
2015-08-04 11:17:18 +0800

    PayType

---
+++ src/com/daoshun/common/PayType.java
@@ -0,0 +1,16 @@
+package com.daoshun.common;
+
+public class PayType {
+	/**
+	 * 余额
+	 */
+	public static int MONEY=1;
+	/**
+	 * 小巴券
+	 */
+    public static int COUPON=2;
+    /**
+     * 小巴币
+     */
+    public static int COIN=3;
+}
