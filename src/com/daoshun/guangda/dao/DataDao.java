270b70bccb33927a9acc4392298d9d716bfd25a2
wugary1985 <gary_wu2010@163.com>
2015-08-05 20:09:41 +0800

    Update DataDao.java
    修改格式

--- src/com/daoshun/guangda/dao/DataDao.java
+++ src/com/daoshun/guangda/dao/DataDao.java
@@ -448,8 +448,7 @@ public class DataDao {
 		SQLQuery query= session.createSQLQuery("{Call updatecoursestate()}");
 		query.executeUpdate();
 	}
-		public List getCoachState(String coachid, int datacount, Date startdate, int starthour, int endhour,
-			int subjectid){
+	public List getCoachState(String coachid, int datacount, Date startdate, int starthour, int endhour,int subjectid){
 		Session session=this.getSession();
 		SQLQuery query= session.createSQLQuery("select getcoachstate(?,?,?,?,?,?)");
 		query.setString(0, coachid);
@@ -458,7 +457,7 @@ public class DataDao {
 		query.setInteger(3, starthour);
 		query.setInteger(4, endhour);
 		query.setInteger(5, subjectid);
-//		query.executeUpdate();
+	//		query.executeUpdate();
 		List list = query.list();
 		return list;
 	}
