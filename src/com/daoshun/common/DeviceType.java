8834ccd9ee2ca84ddd325fdf25276087b6697cf9
laolujava <laolujava@126.com>
2015-08-17 15:17:07 +0800

    设备类型常量

---
+++ src/com/daoshun/common/DeviceType.java
@@ -0,0 +1,20 @@
+package com.daoshun.common;
+/**
+ * 手机设备类型
+ * @author 卢磊
+ *
+ */
+public class DeviceType {
+	/**
+	 * IOS
+	 */
+	public static int IOS=1;
+	/**
+	 * 安卓
+	 */
+    public static int ANDROID=2;
+    /**
+     * 其他类型
+     */
+    public static int OTHER=3;
+}
