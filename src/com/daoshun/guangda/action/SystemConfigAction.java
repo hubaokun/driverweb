dea23b93da48cdde869388ae39180d1495a4ce93
wugary1985 <gary_wu2010@163.com>
2015-08-28 10:02:18 +0800

    遗忘BUG

--- src/com/daoshun/guangda/action/SystemConfigAction.java
+++ src/com/daoshun/guangda/action/SystemConfigAction.java
@@ -612,8 +612,8 @@ public class SystemConfigAction extends BaseAction {
 				systemSetInfo.setCan_use_coupon_count(Integer.parseInt(editvalue));
 			else if(colname.equals("can_use_diff_coupon"))
 				systemSetInfo.setCan_use_diff_coupon(Integer.parseInt(editvalue));
-			else if(colname.equals("advertisement_flag"))
-				systemSetInfo.setAdvertisement_flag(Integer.parseInt(editvalue));
+//			else if(colname.equals("advertisement_flag"))
+//				systemSetInfo.setAdvertisement_flag(Integer.parseInt(editvalue));
 			else if(colname.equals("coursedate_flag"))
 				systemSetInfo.setCoursedate(Integer.parseInt(editvalue));
 			else if(colname.equals("coach_advertisement_flag"))
