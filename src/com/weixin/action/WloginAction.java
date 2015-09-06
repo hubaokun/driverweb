549242869e89b95d9b5a73094c077e89d17d5923
wugary1985 <gary_wu2010@163.com>
2015-09-06 22:08:54 +0800

    新增微信功能

--- src/com/weixin/action/WloginAction.java
+++ src/com/weixin/action/WloginAction.java
@@ -1,22 +1,32 @@
 package com.weixin.action;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.poi.util.SystemOutLogger;
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
@@ -36,25 +46,57 @@ public class WloginAction extends BaseServlet{
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 	}
+	
 	@Override
 	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		String TOKEN = "mZHlAgNp3zqhNh";
-		 // Î¢ĞÅ¼ÓÃÜÇ©Ãû
-        String signature = request.getParameter("signature");
-        // Ëæ»ú×Ö·û´®
-        String echostr = request.getParameter("echostr");
-        // Ê±¼ä´Á
-        String timestamp = request.getParameter("timestamp");
-        // Ëæ»úÊı
-        String nonce = request.getParameter("nonce");
-
-        String[] str = { TOKEN, timestamp, nonce };
-        Arrays.sort(str); // ×ÖµäĞòÅÅĞò
-        String bigStr = str[0] + str[1] + str[2];
-        // SHA1¼ÓÃÜ
-        MessageDigest md;
-        String digest="";
+		if (request.getMethod().equals("GET"))
+		{
+			checksuccess(request,response);
+		}
+		else
+		{
+			InputStream is=request.getInputStream();
+			SAXReader saxReader = new SAXReader();
+			Document document;
+			try {
+				document = saxReader.read(is);
+				Element root = document.getRootElement();
+				List<Element> elements=root.elements();
+				for(Element e:elements)
+				{
+				//	System.out.println(e.getText());
+				}
+				//System.out.println(root.getName());
+			} catch (DocumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			is.close();
+			is = null;
+			
+		}
+		
+        
+	}
+    public void checksuccess(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    	String TOKEN = "mZHlAgNp3zqhNh";
+		 // å¾®ä¿¡åŠ å¯†ç­¾å
+       String signature = request.getParameter("signature");
+       // éšæœºå­—ç¬¦ä¸²
+       String echostr = request.getParameter("echostr");
+       // æ—¶é—´æˆ³
+       String timestamp = request.getParameter("timestamp");
+       // éšæœºæ•°
+       String nonce = request.getParameter("nonce");
+      
+     
+       String[] str = { TOKEN, timestamp, nonce };
+       Arrays.sort(str); // å­—å…¸åºæ’åº
+       String bigStr = str[0] + str[1] + str[2];
+       // SHA1åŠ å¯†
+       MessageDigest md;
+       String digest="";
 		try {
 			md = MessageDigest.getInstance("SHA-1");
 			digest = CommonUtils.byteToString(md.digest(bigStr.getBytes()));
@@ -62,13 +104,12 @@ public class WloginAction extends BaseServlet{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
+       
 
-        // È·ÈÏÇëÇóÀ´ÖÁÎ¢ĞÅ
-        if (digest.equals(signature)) {
-            response.getWriter().print(echostr);
-        }
-     //   response.sendRedirect("weixinWeb/login.jsp");
-	}
+       // ç¡®è®¤è¯·æ±‚æ¥è‡³å¾®ä¿¡
+       if (digest.equals(signature)) {
+           response.getWriter().print(echostr);
+       }
+    }
 	
 }
