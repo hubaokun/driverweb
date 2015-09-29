7fe30dfb8362b9042f1c6f2b465559d9139e2604
wugary1985 <gary_wu2010@163.com>
2015-09-29 17:26:45 +0800

    新增陪驾功能SQL

---
+++ src/sql/9月29日陪驾功能上线需要运行的数据.sql
@@ -0,0 +1,6 @@
+ALTER TABLE app_coach_list
+ADD accompanycoursestate INT(11) NOT NULL DEFAULT '0' ,add orderbyaccompany INT(11) DEFAULT '0';
+
+
+INSERT INTO t_car_models VALUES(19,NOW(),"≈„º›","≈„º›");
+INSERT INTO t_teach_subject VALUES(4,NOW(),"≈„º›");
