00f59e40451dc3d3651a166ade005302ac9a157e
hongcat <44597120@qq.com>
2015-11-05 17:33:13 +0800

    1、获取教练列表getcoachlist方法中增加明星教练优先排序的逻辑。

--- src/com/daoshun/guangda/serviceImpl/SBookServiceImpl.java
+++ src/com/daoshun/guangda/serviceImpl/SBookServiceImpl.java
@@ -1289,7 +1289,7 @@ public class SBookServiceImpl extends BaseServiceImpl implements ISBookService {
 		}else{
 			cuserhql.append(" and usertype=0 ");
 		}
-		cuserhql.append(" and money >= gmoney and isquit = 0  order by coursestate desc,sumnum desc,score desc");
+		cuserhql.append(" and money >= gmoney and isquit = 0  order by signstate desc, coursestate desc,sumnum desc,score desc");
 		//System.out.println(cuserhql.toString());
 		List<AppCuserInfo> coachlist = (List<AppCuserInfo>) dataDao.SqlPageQuery(cuserhql.toString(), Constant.USERLIST_SIZE+1, CommonUtils.parseInt(pagenum, 0) + 1,AppCuserInfo.class, null);
 		if (coachlist != null && coachlist.size() > 0) {
