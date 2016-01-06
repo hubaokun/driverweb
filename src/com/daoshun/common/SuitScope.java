d0704a7d8ab345cf62c795495efbf9e3ccff805e
laolujava <laolujava@126.com>
2016-01-06 17:21:54 +0800

    新增小巴币适用范围类型值

---
+++ src/com/daoshun/common/SuitScope.java
@@ -0,0 +1,10 @@
+package com.daoshun.common;
+
+/**
+ * 小巴币适用范围类型
+ */
+public class SuitScope {
+    public static int PLATFORM=1;
+    public static int DRIVESCHOOL=2;
+    public static int COAH=3;
+}
